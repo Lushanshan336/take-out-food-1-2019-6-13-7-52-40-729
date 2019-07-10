6a54e1f050061a3dd47990b1ef938d62a28b6050
Lushanshan336 <51399970+Lushanshan336@users.noreply.github.com>
2019-07-10 23:37:49 +0800

    Update best-charge.js

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,5 +1,12 @@
 const items = loadAllItems();
 const promotions = loadPromotions();
+function isExsit(itemId) {          //3min
+  for (let i = 0; i < items.length; i++) {
+    if (itemId == items[i].id)
+      return true;
+  }
+  return false;
+}
 
 
 
